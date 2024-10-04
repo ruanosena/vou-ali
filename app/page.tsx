0fd06a57b11b1f43494de57f1298d114b62081a3
ruanosena <ruosena@hotmail.com>
2024-10-04 18:54:05 -0300

    pagina basica

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+import prisma from "@/lib/prisma";
+import { Local } from "./components/Local";
+import { AddLocal } from "./components/AddLocal";
+import { Fragment } from "react";
 
-export default function Home() {
+async function getLocais() {
+  const locais = prisma.local.findMany({
+    where: { Ponto: { isNot: null } },
+    include: { Ponto: { select: { titulo: true } } },
+  });
+  return locais;
+}
+
+export default async function Home() {
+  const locais = await getLocais();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Fragment>
+      <main>
+        <div className="relative flex min-h-screen flex-col items-center justify-center">
+          <label
+            htmlFor="destino"
+            className="absolute left-1/2 top-1/2 block -translate-x-1/2 -translate-y-[calc(50%_+_2rem)] text-xl font-medium leading-6 text-gray-100 sm:text-2xl"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            Pesquisar
+          </label>
+          <div className="relative mt-2 rounded-md shadow-sm">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="text-lg text-gray-500 sm:text-xl">&#x1F4CD;</span>
+            </div>
+            <input
+              type="text"
+              name="destino"
+              id="destino"
+              className="block w-full rounded-md border-0 py-1.5 pl-10 pr-6 text-xl text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-2xl sm:leading-6"
+              placeholder="Nome do lugar"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
+
+        <section className="flex min-h-screen items-center bg-white">
+          <div className="relative mx-auto max-w-7xl flex-col px-4 sm:static sm:px-6 lg:px-8">
+            <div className="sm:max-w-lg">
+              <h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                Adicione seus dados a nosso site
+              </h1>
+              <p className="mt-4 text-lg text-gray-500">
+                Aproxime-se deu seu público de maneira online, sem perder mais clientes por causa de uma pequena
+                informação.
+              </p>
+            </div>
+            <div>
+              <div className="mt-10">
+                <a
+                  href="#"
+                  className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+                >
+                  Informe seu negócio
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+      <footer className="p-2 text-center">
+        Feito com &#x2763; por{" "}
+        <a className="underline underline-offset-2" target="_blank" href="https://github.com/ruanosena">
+          ruanosena
         </a>
       </footer>
-    </div>
+    </Fragment>
   );
 }
